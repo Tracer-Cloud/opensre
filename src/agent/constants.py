f16a92470f87068790a15aa2f736c5db542d7056
davincios <jvf.hus@gmail.com>
2026-01-24 23:24:49 +0000

    remove deadcode

--- src/agent/constants.py
+++ src/agent/constants.py
@@ -3,3 +3,7 @@
 # Tracer Cloud configuration
 TRACER_ORG_ID = "org_33W1pou1nUzYoYPZj3OCQ3jslB2"
 TRACER_BASE_URL = "https://staging.tracer.cloud"
+TRACER_ORG_SLUG = "tracer-bioinformatics"
+
+# Default investigation link (placeholder)
+TRACER_DEFAULT_INVESTIGATION_URL = f"{TRACER_BASE_URL}/{TRACER_ORG_SLUG}/investigations"
